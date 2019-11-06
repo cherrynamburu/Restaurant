e2fe70491401aa8fa897fdea2c9e66a6b4dcd3a6
Cherry <cherrynamburu@gmail.com>
2019-11-06 17:46:08 +0530

    Add Entity Framework Core and Create AppDbContext.cs

--- Restaurant/Startup.cs
+++ Restaurant/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Restaurant.Models;
@@ -14,18 +16,31 @@ namespace Restaurant
 {
     public class Startup
     {
+        
+        private IConfiguration _config;
+
+        // Using Constuctor injection to inject IConfiguration to this class
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // It keeps multiple DbContext objects alive and gives you an unused one rather than creating a new one each time
+            services.AddDbContextPool<AppDbContext>(
+                options => options.UseSqlServer(_config.GetConnectionString("RestaurantDbConnection")));
+
+            // Adds MVC functionality without default routing
             services.AddControllersWithViews();
 
-            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
-
+            // Same instance is provided for each http life cycle.
+            services.AddScoped<IEmployeeRepository, MockEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Order is important as request is processed from top to bottom
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
