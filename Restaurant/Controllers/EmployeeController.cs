1bb8c754ad5233214e7cf0bf9c62c4b8c1482ab9
Cherry <cherrynamburu@gmail.com>
2019-11-08 18:57:16 +0530

    Implementing Edit Action and View On Employee Controller

--- Restaurant/Controllers/EmployeeController.cs
+++ Restaurant/Controllers/EmployeeController.cs
@@ -50,15 +50,7 @@ namespace Restaurant.Controllers
         {
             if (ModelState.IsValid)
             {
-                string uniqueFileName = null;
-                // If the Photo property on the incoming model is not null then the user has selected an image to upload
-                if (model.Photo != null)
-                {
-                    string uploadFolder = Path.Combine(_hostingEnvironment.WebRootPath, "Images");
-                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
-                    string filePath = Path.Combine(uploadFolder, uniqueFileName);
-                    model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
-                }
+                string uniqueFileName = ProcessUploadedFile(model);
                 Employee employee = new Employee()
                 {
                     Name = model.Name,
@@ -71,6 +63,72 @@ namespace Restaurant.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        public ViewResult Edit(int id)
+        {
+            Employee employee = _employeeRepository.GetEmployee(id);
+            EmployeeEditViewModel employeeEditViewModel = new EmployeeEditViewModel()
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                Email = employee.Email,
+                Department = employee.Department,
+                ExistingPhotoPath = employee.PhotoPath
+            };
+            return View(employeeEditViewModel);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(EmployeeEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee employee = _employeeRepository.GetEmployee(model.Id);
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Department = model.Department;
+
+                // If the user wants to change the photo, a new photo will be
+                // uploaded and the Photo property on the model object receives
+                // the uploaded photo. If the Photo property is null, user did
+                // not upload a new photo and keeps his existing photo
+                if (model.Photo != null)
+                {
+                    // If a new photo is uploaded, the existing photo must be deleted. So check if there is an existing photo and delete
+                    if (model.ExistingPhotoPath != null)
+                    {
+                        string filePath = Path.Combine(_hostingEnvironment.WebRootPath,
+                            "images", model.ExistingPhotoPath);
+                        System.IO.File.Delete(filePath);
+                    }
+                    employee.PhotoPath = ProcessUploadedFile(model);
+                }
+                Employee updatedEmployee = _employeeRepository.Update(employee);
+
+                return RedirectToAction("Details", new { id = updatedEmployee.Id});
+            }
+            return View(model);
+        }
+
+        private string ProcessUploadedFile(EmployeeCreateViewModel model)
+        {
+            string uniqueFileName = null;
+            // If the Photo property on the incoming model is not null then the user has selected an image to upload
+            if (model.Photo != null)
+            {
+                string uploadFolder = Path.Combine(_hostingEnvironment.WebRootPath, "Images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                string filePath = Path.Combine(uploadFolder, uniqueFileName);
+                using(var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.Photo.CopyTo(fileStream);
+                }
+            }
+            return uniqueFileName;
+        }
+
 
         // ObjectResult looks at the Request Accept Header and if it is set to application/xml, then XML data is returned.
         // If the Accept header is set to application/json, then JSON data is returned
